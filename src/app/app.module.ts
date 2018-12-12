63f28ab85f5436d1b2fb52487dc63cf648db5815
julia galiulina <92juliegal@gmail.com>
2018-12-12 13:46:26 +0300

    added basic layout for widgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WidgetComponent } from './widget/widget.component';
+import { WidgetListComponent } from './widget-list/widget-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetComponent,
+    WidgetListComponent
   ],
   imports: [
     BrowserModule
